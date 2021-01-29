7f3f5fd08a2f7e3b2efd8a72c26db313c5fc88a0
Signe Sidwall Thygesen <signe.sidwall.thygesen@liu.se>
2021-01-29 16:39:23 +0100

    Change format of input and output to script that generates data for plotting

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -9,7 +9,7 @@ t0 = time.time()
 
 app = inviwopy.app
 network = app.network
-data_folder = "C:/Users/sigsi52/Development/Inviwo/ElectronDensity/data/cpp/cpp_A2B2/"
+data_folder = "C:/Users/sigsi52/Development/Inviwo/ElectronDensity/data/tq/"
 
 # Read metadata file
 f = open(data_folder + "metadata.csv", mode='r')
@@ -17,16 +17,21 @@ lines = f.readlines()
 fileNames = []
 for i in range(1, len(lines)):
     splitted = lines[i].strip().split(",")
-    fileNames.append((splitted[0], splitted[1], splitted[2]))
-
-# Subgroup information
-sumChargeProcessor1 = network.SumChargeInSegmentedRegions
-sumChargeProcessor1.fileLocation.value = data_folder + "subgroups.json"
-sumChargeProcessor2 = network.SumChargeInSegmentedRegions2
-sumChargeProcessor2.fileLocation.value = data_folder + "subgroups.json"
+    # State,Hole cube file,Particle cube file,Subgroups file,Type
+    fileNames.append((splitted[0], splitted[1], splitted[2], splitted[3], splitted[4]))
 
 dataResult = []
+nrSubgroups = 0
 for file in fileNames:
+    # lock
+    app.network.lock()  
+
+    # Subgroups
+    sumChargeProcessor1 = network.SumChargeInSegmentedRegions
+    sumChargeProcessor1.fileLocation.value = data_folder + file[3]
+    sumChargeProcessor2 = network.SumChargeInSegmentedRegions2
+    sumChargeProcessor2.fileLocation.value = data_folder + file[3]
+
     # Hole
     cubeLoader1 = network.CubeSource
     cubeLoader1.cube.value = data_folder + file[1]
@@ -35,20 +40,56 @@ for file in fileNames:
     cubeLoader2 = network.CubeSource2
     cubeLoader2.cube.value = data_folder + file[2]
 
-    #inviwo_utils.update() # Needed for canvas to update
+    # unlock
+    app.network.unlock()
+
+    # wait?
     
+    inviwo_utils.update() # Needed for canvas to update
+
     # Result
     chargeTransferProcessor = network.ComputeChargeTransfer
+        
     holeAndParticleCharges = chargeTransferProcessor.outports[2].getData()
+    chargeDifference = chargeTransferProcessor.outports[0].getData()
     
+    if holeAndParticleCharges == None or chargeDifference == None : 
+        print("Error, no data in outport(s) (None), " + file[4] + ", " + file[0])
+        exit()
+    
+    nrSubgroups = chargeDifference[1].size 
+
     row = []
-    for it in range(0, holeAndParticleCharges[1].size):
-        row.append(holeAndParticleCharges[1].get(it)) 
+    # Type
+    row.append(file[4])
+    # State
+    row.append(file[0])
+    for i in range(0, 2*nrSubgroups):
+        row.append(holeAndParticleCharges[1].get(i)) 
+
+    for j in range(0, nrSubgroups):
+        row.append(chargeDifference[1].get(j))        
 
     dataResult.append(row)
 
-with open(data_folder + 'results.csv', 'w', newline='') as resultsFile:
+holeNames = []
+particleNames = []
+diffNames = []
+for k in range(0, nrSubgroups): 
+    holeNames.append("Hole sg" + str(k+1))
+    particleNames.append("Particle sg" + str(k+1))
+    diffNames.append("Diff sg" + str(k+1))
+
+header = []
+header.append("Name")
+header.append("State")
+header.extend(holeNames)
+header.extend(particleNames)
+header.extend(diffNames)
+
+with open(data_folder + 'results_withDifference2.csv', 'w', newline='') as resultsFile:
     writer = csv.writer(resultsFile)
+    writer.writerow(header)
     writer.writerows(dataResult)
 
 t1 = time.time()
