d941a7d4186a7264464ece0459cc1153926c1fc3
Signe Sidwall Thygesen <signe.sidwall.thygesen@liu.se>
2021-02-09 17:09:54 +0100

    Add selection of feature vector, and dimensionality reduction technique

--- modules/molecularchargetransitions/src/processors/DimensionalityReduction.py
+++ modules/molecularchargetransitions/src/processors/DimensionalityReduction.py
@@ -5,6 +5,8 @@ import ivwdataframe as df
 import numpy as np
 from sklearn.manifold import TSNE
 from sklearn.manifold import MDS
+from sklearn.manifold import Isomap
+from sklearn.decomposition import PCA
 
 class DimensionalityReduction(ivw.Processor):
     def __init__(self, id, name):
@@ -13,6 +15,13 @@ class DimensionalityReduction(ivw.Processor):
         self.addInport(self.inport, owner=False)
         self.outport = df.DataFrameOutport("outport")
         self.addOutport(self.outport)
+
+        self.dimReduction = ivw.properties.OptionPropertyString(
+            "dim_reduction", "Dim. red. technique", [ivw.properties.StringOption("tSNE", "t-SNE", "tSNE"), 
+                                                     ivw.properties.StringOption("MDS", "MDS", "MDS"),
+                                                     ivw.properties.StringOption("isomap", "Isomap", "isomap"),
+                                                     ivw.properties.StringOption("pca", "PCA", "pca")])
+        self.addProperty(self.dimReduction)
         self.perplexity = ivw.properties.FloatProperty(
             "perplexity", "Perplexity", 5.0, 5.0, 50.0, 1.0)
         self.addProperty(self.perplexity)
@@ -22,9 +31,24 @@ class DimensionalityReduction(ivw.Processor):
         self.nIter = ivw.properties.IntProperty(
             "n_iter", "Nr of iterations", 1000, 250, 10000, 50)
         self.addProperty(self.nIter)
-        self.useDifference = ivw.properties.BoolProperty(
-            "use_difference", "Use subgroup difference", False)
-        self.addProperty(self.useDifference)
+        self.neighbors = ivw.properties.IntProperty(
+            "neighbors", "Neighbors (Isomap)", 5, 3, 40, 1)
+        self.addProperty(self.neighbors)
+        self.useOscStrength = ivw.properties.BoolProperty(
+            "use_osc_strength", "Use oscillator strength", False)
+        self.addProperty(self.useOscStrength)
+        self.useRotStrength = ivw.properties.BoolProperty(
+            "use_rot_strenght", "Use rotatory strength", False)
+        self.addProperty(self.useRotStrength)
+        self.useEnergy = ivw.properties.BoolProperty(
+        "use_energy", "Use energy", False)
+        self.addProperty(self.useEnergy)
+
+        self.featureVector = ivw.properties.OptionPropertyString(
+            "feature_vector", "Feature vector", [ivw.properties.StringOption("holeAndParticle", "Hole and particle", "holeAndParticle"), 
+                                                 ivw.properties.StringOption("difference", "Difference (particle - hole)", "difference"), 
+                                                 ivw.properties.StringOption("chargeTransferMatrix", "Charge transfer matrix", "chargeTransferMatrix")])
+        self.addProperty(self.featureVector)
 
     @staticmethod
     def processorInfo():
@@ -53,27 +77,62 @@ class DimensionalityReduction(ivw.Processor):
                 header = inputDataFrame.column(j).header.lower()
 
                 # Use hole and particle subgroup charges
-                if self.useDifference.value == False :
+                if self.featureVector.value == "holeAndParticle":
                     if ("hole" in header) or ("particle" in header):
                         row.append(inputDataFrame.column(j).get(i))
 
                 # Use difference (particle - hole) charges
-                if self.useDifference.value == True :
+                elif self.featureVector.value == "difference":
                     if ("diff" in header):
+                         row.append(inputDataFrame.column(j).get(i))
+
+                # Use charge transfer matrix
+                elif self.featureVector.value == "chargeTransferMatrix":
+                    if ("transfer" in header):
                         row.append(inputDataFrame.column(j).get(i))
+
+                # Add oscillatory strength to feature vector
+                if self.useOscStrength.value == True : 
+                    if ("osc" in header):
+                        row.append(inputDataFrame.column(j).get(i))
+
+                # Add rotatory strength to feature vector
+                if self.useRotStrength.value == True :
+                    if ("rot" in header):
+                        row.append(inputDataFrame.column(j).get(i))
+
+                # Add energy to feature vector
+                if self.useEnergy.value == True :
+                    if ("energy" in header):
+                        row.append(inputDataFrame.column(j).get(i))
+
             X.append(row)
 
         X = np.array(X)
         print("Dimensions before reduction: " + str(X.shape))
 
         # Dimensionality reduction
-        X_embedded = TSNE(n_components=2, perplexity=self.perplexity.value, 
+        if (self.dimReduction.value == "tSNE"):
+            X_embedded = TSNE(n_components=2, perplexity=self.perplexity.value, 
                           learning_rate=self.learningRate.value, n_iter=self.nIter.value).fit_transform(X)
+        elif (self.dimReduction.value == "MDS"):
+            embedding = MDS(n_components=2)
+            X_embedded = embedding.fit_transform(X)
+        elif (self.dimReduction.value == "isomap"):
+            embedding = Isomap(n_components=2, n_neighbors=self.neighbors.value)
+            X_embedded = embedding.fit_transform(X)
+        elif (self.dimReduction.value == "pca"):
+            pca = PCA(n_components=2)
+            pca.fit(X)
+            X_embedded = pca.transform(X)
+
         X_transposed = np.transpose(X_embedded)
-        #embedding = MDS(n_components=2)
-        #X_embedded = embedding.fit_transform(X)
-        X_transposed = np.transpose(X_embedded)
-        
+
+        # Try to make it not "flip" with PCA...
+        # Rotate the data
+        #clf = PCA(n_components=2)
+        #X_transposed = clf.fit_transform(X_transposed)
+
         dataframe = df.DataFrame()
         dataframe.addFloatColumn("1", X_transposed[0])
         dataframe.addFloatColumn("2", X_transposed[1])
